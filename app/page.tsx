bd0bbd3d9a6c0d40e4735bce702d24f4385e7aaf
shysssthanhtri <shyssthanhtri@gmail.com>
2025-11-28 21:32:25 +0700

    Page title

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 const Page = () => {
-  return <h1>Welcome</h1>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev
+        <br />
+        Event you mustn&apos;t miss
+      </h1>
+
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+    </section>
+  );
 };
 
 export default Page;
